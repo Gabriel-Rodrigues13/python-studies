10d52cb6d3ed004f0266da6a7bbf50c6ab803c2d
Gabriel Rodrigues <rodrigues.gabriel1307@gmail.com>
2021-01-04 16:06:00 -0300

    Privando atributos.

--- conta.py
+++ conta.py
@@ -2,18 +2,18 @@
 
 class Conta:
     def __init__(self, numero, titular, saldo, limite = 1000.0):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     
     def extrato(self):
-        print(f"O saldo da conta {self.numero} é de {self.saldo}.")
+        print(f"O saldo da conta {self.__numero} é de {self.__saldo}.")
     
     def deposita(self, valor):
-        self.saldo += valor
-        print(f"O valor de {valor} foi depositado na conta {self.numero}. O novo saldo é de {self.saldo}")
+        self.__saldo += valor
+        print(f"O valor de {valor} foi depositado na conta {self.__numero}. O novo saldo é de {self.__saldo}")
 
     def saca(self, valor):
-        self.saldo -= valor
-        print(f"O valor de {valor} foi sacado da conta {self.numero}. O novo saldo é de {self.saldo}")
+        self.__saldo -= valor
+        print(f"O valor de {valor} foi sacado da conta {self.__numero}. O novo saldo é de {self.__saldo}")
