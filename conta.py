66a5302617886d5bd0a9b55efa83130164975c3b
Gabriel Rodrigues <rodrigues.gabriel1307@gmail.com>
2021-01-04 16:41:45 -0300

    Getters e Setters

--- conta.py
+++ conta.py
@@ -2,6 +2,7 @@
 
 class Conta:
     def __init__(self, numero, titular, saldo, limite = 1000.0):
+        print(f"Construindo objeto...{self}")
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
@@ -17,3 +18,19 @@ class Conta:
     def saca(self, valor):
         self.__saldo -= valor
         print(f"O valor de {valor} foi sacado da conta {self.__numero}. O novo saldo é de {self.__saldo}")
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limit(self, limite):
+        self.__limite = limite
