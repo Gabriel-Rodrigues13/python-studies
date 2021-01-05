91c757c5dd69ffca1a27451979b19daf898936b5
Gabriel Rodrigues <rodrigues.gabriel1307@gmail.com>
2021-01-05 15:23:15 -0300

    Adicionando métodos privados

--- conta.py
+++ conta.py
@@ -9,15 +9,22 @@ class Conta:
         self.__limite = limite
     
     def extrato(self):
-        print(f"O saldo da conta {self.__numero} é de {self.__saldo}.")
+        print(f"O saldo da conta {self.__numero} é de {self.__saldo}. Seu limite é {self.limite}")
     
     def deposita(self, valor):
         self.__saldo += valor
         print(f"O valor de {valor} foi depositado na conta {self.__numero}. O novo saldo é de {self.__saldo}")
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_sacar = self.saldo + self.limite
+        return valor_a_sacar <= valor_disponivel_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
-        print(f"O valor de {valor} foi sacado da conta {self.__numero}. O novo saldo é de {self.__saldo}")
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+            print(f"O valor de {valor} foi sacado da conta {self.__numero}. O novo saldo é de {self.__saldo}")
+        else:
+            print(f'O valor {valor} passou o limite')
 
     def transfere(self, valor, destino):
         self.saca(valor)
