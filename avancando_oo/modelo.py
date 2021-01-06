d7172fc0bd64e897989e899837b76065f29d8e52
Gabriel Rodrigues <rodrigues.gabriel1307@gmail.com>
2021-01-06 12:06:12 -0300

    Reduzindo ifs

--- avancando_oo/modelo.py
+++ avancando_oo/modelo.py
@@ -21,6 +21,9 @@ class Programa():
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def imprime(self):
+        print(f"{self._nome} - {self.ano} - {self._likes} Likes")
+
 
 class Filme(Programa):
     
@@ -28,25 +31,30 @@ class Filme(Programa):
         super().__init__(nome,ano)
         self.duracao = duracao
 
+    def imprime(self):
+        print(f"{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes")
+
 class Serie(Programa):
     
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome,ano)
         self.temporadas = temporadas
 
+    def imprime(self):
+        print(f"{self._nome} - {self.temporadas} temporadas - {self._likes} Likes")
+
+
+
 
 vingadores = Filme("Vingadores - Guerra Infinita", 2018, 160)
 vingadores.dar_like()
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração {vingadores.duracao} - Likes {vingadores.likes}")
 
 
 atlanta = Serie("Atlanta", 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas {atlanta.temporadas} - Likes {atlanta.likes}")
 
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa,'duracao') else programa.temporadas
-    print(f"{programa.nome} - {detalhes} - {programa.likes}")
+    programa.imprime()
