aa31d82025238549dedddb7dab8d6dc3732759f8
Gabriel Rodrigues <rodrigues.gabriel1307@gmail.com>
2021-01-07 16:20:20 -0300

    Implementando o __getitem__

--- avancando_oo/modelo.py
+++ avancando_oo/modelo.py
@@ -44,11 +44,22 @@ class Serie(Programa):
         return f"{self._nome} - {self.temporadas} temporadas - {self._likes} Likes"
 
 
-class Playlist(list):
+class Playlist:
 
     def __init__(self, nome, programas):
         self._nome = nome.title()
-        super().__init__(programas)
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+    
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 
 vingadores = Filme("Vingadores - Guerra Infinita", 2018, 160)
@@ -72,9 +83,7 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}')
 
 for programa in playlist_fim_de_semana:
     print(programa)
-
-print(f"Tá ou nao está: {demolidor in playlist_fim_de_semana}")
